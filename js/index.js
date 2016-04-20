32a56a282567172a90334e4b395f771d76a0880a
Rocky <luoqrocky@gmail.com>
2016-04-20 22:34:15 +0800

    some modification for electron, add some UI use nodejs module to finish download

--- js/index.js
+++ js/index.js
@@ -2,55 +2,24 @@
  * download
  */
 
-  //get pattern list at the first time
-  var patternList = [];
-  $.ajax({
-    url:"",
-    type:"get",
-    dataType:"json",
-    success: function(data) {
-      if(data.successed) {
-        patternList.push(data);
-      }
-    }
-  });
-
-
-  //$('.modal-trigger').leanModal();
-
-document.getElementById("download").addEventListener('click',function(e) {
-  e.preventDefault();
-
-  //downloadFile('data.zip','http://192.168.1.105:8080/gs-robot/data/map_png?map_name=ssc6');
-  downloadFile('data.zip','https://github.com/Rockyluoqi/algs4/archive/master.zip');
-
-  //var xhr = new XMLHttpRequest();
-  //xhr.open("POST", baseURLDownload + "/service/report/QCPReport", true);
-  //xhr.setRequestHeader("Content-type","application/json");
-  //xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-  //xhr.onreadystatechange = function() {
-  //  if (xhr.readyState == 4 && xhr.status == 200) {
-  //    // alert("Failed to download:" + xhr.status + "---" + xhr.statusText);
-  //    var blob = new Blob([xhr.response], {type: "octet/stream"});
-  //    var fileName = "QCPReport.zip";
-  //    saveAs(blob, fileName);
-  //  }
-  //}
-  //xhr.responseType = "arraybuffer";
-  //xhr.send(JSON.stringify(QCPParameter));
-
-  //$.fileDownload('http://192.168.1.105:8080/gs-robot/data/map_png?map_name=ssc6');
-  //$.fileDownload('http://192.168.1.105:8080/gs-robot/data/map_png?map_name=ssc6',{
-  //  successCallback: function(url) {
-  //    alert('You just got a file download dialog or ribbon for this URL :' + url);
-  //  },
-  //  failCallback: function(html,url) {
-  //    alert('Your file download just failed for this URL:' + url + '\r\n' +
-  //      'Here was the resulting error HTML: \r\n' + html
-  //    );
-  //  }
-  //});
-});
+$('.modal-trigger').leanModal();
+//get pattern list at the first time
+//var patternList = [];
+//$.ajax({
+//  url:"",
+//  type:"get",
+//  dataType:"json",
+//  success: function(data) {
+//    if(data.successed) {
+//      patternList.push(data);
+//    }
+//  }
+//});
+//document.getElementById("download").addEventListener('click',function(e) {
+//  e.preventDefault();
+//
+//  downloadFile('data.zip','https://github.com/Rockyluoqi/FlowGraph/archive/master.zip');
+//});
 //
 function downloadFile(fileName, urlData){
   //var aLink = document.createElement('a');
@@ -59,9 +28,8 @@ function downloadFile(fileName, urlData){
   //aLink.download = fileName;
   //aLink.href = urlData;
   //aLink.dispatchEvent(evt);
+
   $("#progressBar").css("visibility", "visible");
-
-
   var request = require('request'),
       fs = require('fs'),
       zlib = require('zlib'),
@@ -71,77 +39,51 @@ function downloadFile(fileName, urlData){
   console.log('filename: ' + file_name);
   var out = fs.createWriteStream('./download/' + file_name);
 
-  request(urlData).pipe(out);
-
-  //$.ajax({
-  //  url:urlData,
-  //  type:'GET',
-  //  //success:function(data) {
-  //  //  //alert(data);
-  //  //
-  //  //}
-  //  success:function(data) {
-  //
-  //  },
-  //  complete: function(data) {
-  //    if(data.status === 200) {
-  //      out.write(data);
-  //      out.end();
-  //      console.log(data);
-  //    }
-  //      //data.pipe(zlib.createGunZip()).pipe(out);
-  //    $("#progressBar").css("visibility", "hidden");
-  //  }
-  //});
+  var len;
+  //use 'request'
+  request
+      .get(urlData)
+      .on('response', function(response) {
+        console.log(response.statusCode) // 200
+        console.log(response.complete);
+        console.log(response.headers['content-type']) // 'image/png'
+        console.log(response.headers['content-length'])
+        var len = response.headers['content-length'];
+      })
+      .pipe(out)
+      .on('finish', function() {
+        console.log("finish.....");
+        $("#progressBar").css("visibility", "hidden");
+        Materialize.toast(file_name +' is downloaded !', 4000);
+      });
 }
-
-
-
-//var xhr = new XMLHttpRequest();
-//xhr.open("POST", baseURLDownload + "/service/report/QCPReport", true);
-//xhr.setRequestHeader("Content-type","application/json");
-//xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-//xhr.onreadystatechange = function() {
-//  if (xhr.readyState == 4 && xhr.status == 200) {
-//    // alert("Failed to download:" + xhr.status + "---" + xhr.statusText);
-//    var blob = new Blob([xhr.response], {type: "octet/stream"});
-//    var fileName = "QCPReport.zip";
-//    saveAs(blob, fileName);
-//  }
-//}
-//xhr.responseType = "arraybuffer";
-//xhr.send(JSON.stringify(QCPParameter));
-
-
-//downloadFile('urlName', 'url');
 
 //var $upload = $('#upload');
 //$upload.('change',onFileInputChange,false);
 
-  //document.getElementById('download').addEventListener('click', function () {
-  //  var list = document.createElement("form");
-  //  list.setAttribute('action', "#");
-  //  for(var i=0;i<patternList.length;i++) {
-  //    var p = document.createElement('p');
-  //    var input = document.createElement('input');
-  //    input.setAttribute('name', 'group1');
-  //    input.setAttribute('type', 'radio');
-  //    input.setAttribute('id', 'test' + i);
-  //    var label = document.createElement('label');
-  //    label.setAttribute('for', 'test' + i);
-  //    label.appendChild(patternList[i]);
-  //    p.appendChild(input);
-  //    p.appendChild(label);
-  //  }
-  //});
+document.getElementById('download').addEventListener('click', function () {
+  var list = document.createElement("form");
+  list.setAttribute('action', "#");
+  for(var i=0;i<patternList.length;i++) {
+    var p = document.createElement('p');
+    var input = document.createElement('input');
+    input.setAttribute('name', 'group1');
+    input.setAttribute('type', 'radio');
+    input.setAttribute('id', 'test' + i);
+    var label = document.createElement('label');
+    label.setAttribute('for', 'test' + i);
+    label.appendChild(patternList[i]);
+    p.appendChild(input);
+    p.appendChild(label);
+  }
+});
 
-  //document.getElementById('downloadSubmit').addEventListener('click',function() {
-  //  var robotPattern = $('input[name="group1"]:checked').val();
-  //  downloadFile(robotPattern,"url"+robotPattern+".zip");
-  //});
+document.getElementById('downloadSubmit').addEventListener('click',function() {
+  var robotPattern = $('input[name="group1"]:checked').val();
+  downloadFile(robotPattern,"url"+robotPattern+".zip");
+});
 
-
-
+//show selected files in a list
 $('input[type=file]').change(function() {
   var form = document.forms["uploadForm"];
   if (form["file"].files.length > 0) {
@@ -149,6 +91,15 @@ $('input[type=file]').change(function() {
     $("#uploadBtn").css("color", "#FFFFFF");
     var files = form["file"].files;
     var ul = document.getElementById('listContainer');
+
+    clearList();
+
+    var li_1 = document.createElement('li');
+    li_1.setAttribute('class', 'collection-item');
+    var h = document.createElement('h5');
+    h.innerText = "Upload file list";
+    li_1.appendChild(h);
+    ul.appendChild(li_1);
 
     for(var i=0;i<files.length;i++) {
       var li = document.createElement('li');
@@ -174,101 +125,150 @@ $('input[type=file]').change(function() {
   }
 });
 
-  function uploadAndSubmit() {
-    var form = document.forms["uploadForm"];
-    var fileName = $("[name='file']#fileID").val().split('\\').pop();
-    //var fileName= $("[name='file']#fileID").val();
-    console.log(fileName);
-    if (form["file"].files.length > 0) {
+function clearList() {
+  var list = document.getElementById('listContainer');
+  list.innerHTML = "";
+}
 
-      // 寻找表单域中的 <input type="file" ... /> 标签
-      var file = form["file"].files[0];
-      var files = form["file"].files;
+function uploadAndSubmit() {
+  event.preventDefault();
+  var form = document.forms["uploadForm"];
+  var fileName = $("[name='file']#fileID").val().split('\\').pop();
+  //var fileName= $("[name='file']#fileID").val();
+  console.log(fileName);
+  $("#progressBar").css("visibility", "visible");
+  if (form["file"].files.length > 0) {
 
-      console.log(files);
-      // try sending
-      var reader = new FileReader();
+    // 寻找表单域中的 <input type="file" ... /> 标签
+    var files = form["file"].files;
 
-      reader.onloadstart = function () {
-        // 这个事件在读取开始时触发
-        console.log("onloadstart");
-        document.getElementById("bytesTotal").textContent = file.size;
-        $("#progressBar").css("visibility", "visible");
-      };
-      reader.onprogress = function (p) {
-        // 这个事件在读取进行中定时触发
-        console.log("onprogress");
-        document.getElementById("bytesRead").textContent = p.loaded;
-      };
+    var formData = new FormData();
 
-      reader.onload = function () {
-        // 这个事件在读取成功结束后触发
-        console.log("load complete");
-      };
+    for(var i=0;i<files.length;i++) {
+      var file = files[i];
 
-      reader.onloadend = function () {
-        // 这个事件在读取结束后，无论成功或者失败都会触发
-        if (reader.error) {
-          console.log(reader.error);
-        } else {
-          document.getElementById("bytesRead").textContent = file.size;
-          // 构造 XMLHttpRequest 对象，发送文件 Binary 数据
-          for(var i=0;i<files.length;i++) {
-            //or use ajax
-
-            var xhr = new XMLHttpRequest();
-            xhr.open(/* method */ "POST",
-              /* target url */ "upload.jsp?fileName=" + file.name
-              /*, async, default to true */);
-            xhr.overrideMimeType("application/octet-stream");
-            var zipBinaryBytes = new Uint8Array(reader.result.length);
-            for (var i = 0; i < reader.result.length; ++i) {
-              zipBinaryBytes[i] = reader.result.charCodeAt(i);
-            }
-            var zipBinary = new Blob([zipBinaryBytes], {type: 'application/zip'});
-
-            //sendData(zipBinary);
-            xhr.send(zipBinary);
-            console.log(zipBinary);
-            xhr.onreadystatechange = function () {
-              if (xhr.readyState == 4) {
-                if (xhr.status == 200) {
-                  console.log("upload complete");
-                  console.log("response: " + xhr.responseText);
-                  $("#progressBar").css("visibility", "hidden");
-                }
-              }
-            }
-          }
-        }
-      };
-      reader.readAsBinaryString(file);
-    } else {
-      alert("Please choose a file.");
-      $("#uploadBtn").css("background-color", "#DFDFDF");
-      $("#uploadBtn").css("color", "#9F9F9F");
+      formData.append(file.name, file);
     }
+
+    var xhr = new XMLHttpRequest();
+    (xhr.upload || xhr).addEventListener('progress', function(e) {
+      var done = e.position || e.loaded;
+      var total = e.totalSize || e.total;
+      console.log('xhr progress: ' + Math.round(done/total*100) + '%');
+
+      //example update the progress data
+      $('#progress1').innerText = Math.round(done/total*100) + '%';
+    });
+    // 请求完成时建立一个处理程序。
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        // File(s) uploaded.
+        uploadButton.innerHTML = 'Upload';
+      } else {
+        alert('An error occurred!');
+      }
+    };
+
+    xhr.open();
+    xhr.send(formData);
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+          console.log("upload complete");
+          console.log("response: " + xhr.responseText);
+          $("#progressBar").css("visibility", "hidden");
+        }
+      }
+    };
+
+    //console.log(files);
+    //// try sending
+    //for(var i=0;i<files.length;i++) {
+    //  var file = files[i];
+    //  var reader = new FileReader();
+    //
+    //  reader.onloadstart = function () {
+    //    // 这个事件在读取开始时触发
+    //    console.log("onloadstart");
+    //    document.getElementById("bytesTotal").textContent = file.size;
+    //    $("#progressBar").css("visibility", "visible");
+    //  };
+    //  reader.onprogress = function (p) {
+    //    // 这个事件在读取进行中定时触发
+    //    console.log("onprogress");
+    //    document.getElementById("bytesRead").textContent = p.loaded;
+    //  };
+    //
+    //  reader.onload = function () {
+    //    // 这个事件在读取成功结束后触发
+    //    console.log("load complete");
+    //  };
+    //
+    //  reader.onloadend = function () {
+    //    // 这个事件在读取结束后，无论成功或者失败都会触发
+    //    if (reader.error) {
+    //      console.log(reader.error);
+    //    } else {
+    //      document.getElementById("bytesRead").textContent = file.size;
+    //      // 构造 XMLHttpRequest 对象，发送文件 Binary 数据
+    //      for (var i = 0; i < files.length; i++) {
+    //        //or use ajax
+    //
+    //        var xhr = new XMLHttpRequest();
+    //        xhr.open(/* method */ "POST",
+    //            /* target url */ "upload.jsp?fileName=" + file.name
+    //            /*, async, default to true */);
+    //        xhr.overrideMimeType("application/octet-stream");
+    //        var zipBinaryBytes = new Uint8Array(reader.result.length);
+    //        for (var i = 0; i < reader.result.length; ++i) {
+    //          zipBinaryBytes[i] = reader.result.charCodeAt(i);
+    //        }
+    //        var zipBinary = new Blob([zipBinaryBytes], {type: 'application/zip'});
+    //
+    //        //sendData(zipBinary);
+    //        xhr.send(zipBinary);
+    //        console.log(zipBinary);
+    //        xhr.onreadystatechange = function () {
+    //          if (xhr.readyState == 4) {
+    //            if (xhr.status == 200) {
+    //              console.log("upload complete");
+    //              console.log("response: " + xhr.responseText);
+    //              $("#progressBar").css("visibility", "hidden");
+    //            }
+    //          }
+    //        }
+    //      }
+    //    }
+    //  };
+    //  reader.readAsBinaryString(file);
+    //}
+  } else {
+    alert("Please choose a file.");
+    $("#uploadBtn").css("background-color", "#DFDFDF");
+    $("#uploadBtn").css("color", "#9F9F9F");
   }
+}
 
 
-  /*function sendData(zipBinary) {
-   var zipBinaryBytes = new Uint8Array(zipBinary.length);
-   for (var i = 0; i < zipBinary.length; ++i) {
-   zipBinaryBytes[i] = zipBinary.charCodeAt(i);
-   }
-   zipBinary = new Blob([zipBinaryBytes], { type: 'application/zip' });
-   // rest of code as I suggested above...
+/*function sendData(zipBinary) {
+ var zipBinaryBytes = new Uint8Array(zipBinary.length);
+ for (var i = 0; i < zipBinary.length; ++i) {
+ zipBinaryBytes[i] = zipBinary.charCodeAt(i);
+ }
+ zipBinary = new Blob([zipBinaryBytes], { type: 'application/zip' });
+ // rest of code as I suggested above...
 
-   //fd.append('hello.zip', zipBinary);
-   //xhr.onload = function() {
-   //   Request completed! Use xhr.status and/or xhr.responseText to
-   //   check the server's response status code and response body.
-   //};
-   //xhr.onerror = function() {
-   //   Aw. Network error.
-   //};
-   //xhr.open('POST', 'https://example.com/');
-   //xhr.send(fd);
-   console.log(zipBinary);
-   //xhr.send(new TextEncoder().encode(prefix+zipBinary+p))
-   }*/
+ //fd.append('hello.zip', zipBinary);
+ //xhr.onload = function() {
+ //   Request completed! Use xhr.status and/or xhr.responseText to
+ //   check the server's response status code and response body.
+ //};
+ //xhr.onerror = function() {
+ //   Aw. Network error.
+ //};
+ //xhr.open('POST', 'https://example.com/');
+ //xhr.send(fd);
+ console.log(zipBinary);
+ //xhr.send(new TextEncoder().encode(prefix+zipBinary+p))
+ }*/
